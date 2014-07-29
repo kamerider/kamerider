d735852997e50fad2623e6f4e029ac5e075024c2
Seow Yip Loon <seow@seow-Satellite-M840.(none)>
2014-07-29 16:37:09 +0900

    TUTORIAL

---
+++ TUTORIAL/BASIC_TUTORIAL/colour_tracking/src/colour_tracker.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+
+"""
+     colour_tracker.py - follow the color desired
+
+"""
+
+import roslib; roslib.load_manifest('cmvision')
+import rospy
+from cmvision.msg import Blobs
+
+blob_position_x = 0
+
+class Colour_Tracker:
+    def __init__(self):
+        rospy.on_shutdown(self.cleanup)
+
+    	#subscribe to the robot sensor state
+    	rospy.Subscriber('/blobs', Blobs, self.callback)
+
+    	while not rospy.is_shutdown():
+	    rospy.loginfo("blob is at x: %s"%blob_position_x)
+
+    def callback(self, data):
+        if(len(data.blobs)):
+	    global blob_position_x
+
+	    blob_position_x = 0
+
+	    for obj in data.blobs:
+	      blob_position_x = blob_position_x + obj.x
+
+	    blob_position_x = blob_position_x/len(data.blobs)
+
+    def cleanup(self):
+        rospy.loginfo("Shutting down color tracking...")
+
+if __name__=="__main__":
+    rospy.init_node('colour_tracker')
+    try:
+        Colour_Tracker()
+        rospy.spin()
+    except rospy.ROSInterruptException:
+        pass
+
---
+++ TUTORIAL/INTERMEDIATE_TUTORIAL/digital_input_output_2/src/digital_input_output_2.py
@@ -0,0 +1,137 @@
+#!/usr/bin/env python
+
+import roslib; roslib.load_manifest('kobuki_testsuite');
+import rospy
+
+# Include sound client to produce speech
+from sound_play.libsoundplay import SoundClient
+
+from std_msgs.msg import String
+from kobuki_msgs.msg import DigitalInputEvent, Led, ButtonEvent, BumperEvent, DigitalOutput
+
+digital_in = [True, True, True, True]
+button = 0
+button_state = 0
+bumper = 0
+bumper_state = 0
+
+class MainLoop:
+  def __init__(self):
+    rospy.on_shutdown(self.cleanup)
+
+    self.voice = rospy.get_param("~voice", "voice_don_diphone")
+    self.wavepath = rospy.get_param("~wavepath", "")
+
+    # Create the sound client object
+    self.soundhandle = SoundClient()
+
+    rospy.sleep(1)
+    self.soundhandle.stopAll()
+
+    # Subscribe to the /mobile_base/events/digital_input topic to receive digital input
+    rospy.Subscriber('/mobile_base/events/digital_input', DigitalInputEvent, self.DigitalInputEventCallback)
+
+    # Subscribe to the /mobile_base/events/button topic to receive built-in button input
+    rospy.Subscriber('/mobile_base/events/button', ButtonEvent, self.ButtonEventCallback)
+
+    # Subscribe to the /mobile_base/events/bumper topic to receive bumper input
+    rospy.Subscriber('/mobile_base/events/bumper', BumperEvent, self.BumperEventCallback) 
+
+    # publish Led messages to /mobile_base/commands/led1 & /mobile_base/commands/led2 topics to give Led signal
+    self.pub1 = rospy.Publisher('/mobile_base/commands/led1', Led)
+    self.pub2 = rospy.Publisher('/mobile_base/commands/led2', Led)
+    self.led1 = Led()
+    self.led2 = Led()
+
+    # publish Led messages to /mobile_base/commands/digital_output to give digital signal
+    self.pub3 = rospy.Publisher('/mobile_base/commands/digital_output', DigitalOutput)
+    self.digital_out = DigitalOutput()
+
+    rospy.sleep(1)
+    # Announce user that the robot is ready
+    self.soundhandle.say("Ready", self.voice)
+
+    # while loop
+    while not rospy.is_shutdown():
+
+      if digital_in[1]==True:
+	rospy.loginfo("button 1 pressed !!!")
+
+      elif digital_in[2]==True:
+	rospy.loginfo("button 2 pressed !!!")
+
+      elif digital_in[3]==True:
+	rospy.loginfo("button 3 pressed !!!")
+
+
+
+      if bumper == 0:
+	if bumper_state == 1:
+	  self.led1.value = 1
+
+      elif bumper == 1:
+	if bumper_state == 1:
+	  self.led1.value = 2
+
+      elif bumper == 2:
+	if bumper_state == 1:
+	  self.led1.value = 3
+
+
+
+      if button == 0:
+	if button_state == 1:
+	  self.led1.value = 1
+          self.digital_out.values = [False, False, False, False]
+          self.digital_out.mask = [True, True, True, True]
+
+	  self.soundhandle.say("button 1 is pressed", self.voice)
+          rospy.sleep(1)
+
+      elif button == 1:
+	if button_state == 1:
+	  self.led1.value = 2
+          self.digital_out.values = [True, True, True, True]
+          self.digital_out.mask = [True, True, True, True]
+
+	  self.soundhandle.say("button 2 is pressed", self.voice)
+          rospy.sleep(1)
+
+      elif button == 2:
+	if button_state == 1:
+	  self.led1.value = 3
+
+	  self.soundhandle.say("button 3 is pressed", self.voice)
+          rospy.sleep(1)
+
+
+      self.pub1.publish(self.led1)
+      self.pub2.publish(self.led2)
+      self.pub3.publish(self.digital_out)
+      rospy.Rate(4).sleep()
+
+  def cleanup(self):
+    rospy.loginfo("Stopping digital IO checking...")
+
+  def DigitalInputEventCallback(self,data):
+    global digital_in
+    digital_in = data.values
+
+  def ButtonEventCallback(self,data1):
+    global button
+    global button_state
+    button = data1.button
+    button_state = data1.state
+
+  def BumperEventCallback(self,data2):
+    global bumper
+    global bumper_state
+    bumper = data2.bumper
+    bumper_state = data2.state
+
+if __name__ == '__main__':
+  rospy.init_node('digital_IO')
+  try:
+	MainLoop()
+  except rospy.ROSInterruptException:
+  	pass
